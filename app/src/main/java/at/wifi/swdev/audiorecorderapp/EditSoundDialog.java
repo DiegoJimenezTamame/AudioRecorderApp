f1cd8d6b9d3c2c4e8b077f8553f8f73a2238bb7d
diegojimeneztamame <djimeneztamame@gmail.com>
2024-03-30 11:02:21 +0100

    Implemented the color palette button in the EditSoundDialog, but the OnClickListener still doesn't work, the logic has to be implemented. Edited the colors in the "Enable Edit" toggle and changed its logic to ImageButton Created several .xml layout files and resources. Created icon resources. TO DO: Loop function still doesn't work. TO DO: Continue editing Dialog functions, create color categories and on-the-fly button color selection. TO DO: Continue implementation of audio editing methods. TO DO: Research setting up of Sample rate and audio attributes.

--- app/src/main/java/at/wifi/swdev/audiorecorderapp/EditSoundDialog.java
+++ app/src/main/java/at/wifi/swdev/audiorecorderapp/EditSoundDialog.java
@@ -2,11 +2,10 @@ package at.wifi.swdev.audiorecorderapp;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ToggleButton;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +19,41 @@ public class EditSoundDialog extends DialogFragment {
     private boolean isLooping = false;
     private boolean loopingState = false; //Default looping state
 
+    private final String[] colorOptions = {
+            "Blue", "Green", "Pink", "Orange", "Purple"
+    };
+
+    private final int[] colorResources = {
+            R.drawable.blue_button,
+            R.drawable.green_button,
+            R.drawable.pink_button,
+            R.drawable.orange_button,
+            R.drawable.purple_button
+    };
+
+    public interface ColorSelectionListener{
+        void onColorSelected(int colorResId);
+    }
+    // Declare listener variable
+    private ColorSelectionListener colorSelectionListener;
+
+    // Set listener method
+    public void setColorSelectionListener(ColorSelectionListener listener) {
+        this.colorSelectionListener = listener;
+    }
+
+    // When a color is selected, notify the listener
+    private void notifyColorSelected(int colorResId) {
+        if (colorSelectionListener != null) {
+            colorSelectionListener.onColorSelected(colorResId);
+        }
+    }
+
+    // This method will be called when a color is selected
+    private void onColorItemSelected(int colorResId) {
+        // Notify the listener with the selected color
+        notifyColorSelected(colorResId);
+    }
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -28,12 +62,26 @@ public class EditSoundDialog extends DialogFragment {
         View dialogView = inflater.inflate(R.layout.dialog_edit_sound, null);
 
         // Find the toggle button for looping
-        ToggleButton loopToggleButton = dialogView.findViewById(R.id.loop_toggle_button);
-        loopToggleButton.setChecked(isLooping); // Set initial state
+        ImageButton loopImageButton = dialogView.findViewById(R.id.loop_image_button);
+        updateLoopingButtonState(loopImageButton);
+        loopImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle the state
+                isLooping = !isLooping;
+                updateLoopingButtonState(loopImageButton);
+            }
+        }); // Set initial state
 
-        loopToggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            isLooping = isChecked; // Update looping state
-        });
+        /*ImageButton colorButton = dialogView.findViewById(R.id.color_palette);
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open a new dialog for selecting colors
+                showColorSelectionDialog();
+            }
+        });*/
+
 
         builder.setView(dialogView)
                 .setPositiveButton("Apply", (dialog, id) -> applyChanges())
@@ -53,7 +101,17 @@ public class EditSoundDialog extends DialogFragment {
 
     }
 
+
+
     public void setLooping(boolean looping){
         loopingState = looping;
+    }
+
+    public void updateLoopingButtonState(ImageButton button){
+        if (isLooping) {
+            button.setImageResource(R.drawable.ic_loop_on); // Change to the looping icon
+        }else{
+            button.setImageResource(R.drawable.ic_loop_off);
+        }
     }
 }
